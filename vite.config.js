f4d050a304539b04bab4878733953a0277fe11db
jorgediazklu <jorge.diaz@klu.mx>
2023-05-02 14:40:36 -0500

    bitbuicket-7

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 /*
 // https://vitejs.dev/config/
@@ -18,10 +15,6 @@ export default ({ mode }) => {
   return defineConfig({
     // To access env vars here use process.env.TEST_VAR
     plugins: [react()],
-    env: {
-      AWS_ACCESS_KEY_ID: process.env.VITE_AWS_ACCESS_KEY_ID,
-      AWS_SECRET_ACCESS_KEY: process.env.VITE_AWS_SECRET_ACCESS_KEY,
-      AWS_DEFAULT_REGION: process.env.VITE_AWS_DEFAULT_REGION,
-    },
+    env,
   });
 };
